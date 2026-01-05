dc48f938e104cf7bbe155db7409d330a52d2b288
jaider <jaider@unergy.io>
2026-01-05 17:49:19 -0500

    v1.0.0 refactor(main): simplify main function and remove unused code
    Updated the main function to fetch and print content from a URL, removing the previous Lissajous animation code and related imports.

--- main.go
+++ main.go
@@ -1,57 +1,22 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/gif"
+	"fmt"
 	"io"
-	"math"
-	"math/rand"
-	"os"
-)
-
-var palette = []color.Color{color.White, color.Black}
-
-const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	"net/http"
 )
 
 func main() {
-	lissajous(os.Stdout)
-}
-
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
-
-	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
-
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-
-		for t := 0.0; t < cycles*3*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(
-				size+int(x*size+0.5),
-				size+int(y*size+0.5),
-				blackIndex,
-			)
-		}
-
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+	url := "http://gopl.io"
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
 	}
 
-	_ = gif.EncodeAll(out, &anim) // ignoring encoding errors
+	b, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", b)
 }
